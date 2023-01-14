8eb1b2f3bf2ba2c32cd772cfc7ae657a9394194b
lesley-byte <phosphorescence3@hotmail.com>
2023-01-14 15:07:35 -0500

    messing with routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
-const router = require('express').Router();
-const apiRoutes = require('./api');
+const router = require("express").Router();
+const apiRoutes = require("./api");
 
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
 
 router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
+  // If no API routes are hit, send error message
+  res.send(
+    `<h1>Wrong Route! <strong> ${req.url} </strong> does not work!</h1>`
+  );
 });
 
 module.exports = router;

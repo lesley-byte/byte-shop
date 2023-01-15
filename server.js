07cdb77fc73fe800324e745c8997d54862875110
lesley-byte <phosphorescence3@hotmail.com>
2023-01-14 21:11:59 -0500

    added splash screen

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const { clog } = require("./middleware/clog");
+const logo = require("asciiart-logo");
 // import sequelize connection
 const sequelize = require("./config/connection");
 
@@ -15,5 +16,22 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`App listening on port ${PORT}!🚀`));
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!🚀`),
+      console.log(
+        logo({
+          name: "Byte Shop",
+          font: "Electronic",
+          lineChars: 10,
+          padding: 2,
+          margin: 3,
+          borderColor: "bold-cyan",
+          logoColor: "bold-blue",
+          textColor: "green",
+        })
+          .emptyLine()
+          .right(`Open for business on port ${PORT}!🚀`)
+          .render()
+      );
+  });
 });
